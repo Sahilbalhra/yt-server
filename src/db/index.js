763ecaf2dfb835bc2930de2c0ead4d77e5fedd02
Sahilbalhra <sahilbalhra0@gmail.com>
2024-01-17 00:33:59 +0530

    api middleware added

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB=async()=>{
     try {
        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
-       console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
+       console.info(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error("MongoDB connection Failed :" , error)
         process.exit(1)
